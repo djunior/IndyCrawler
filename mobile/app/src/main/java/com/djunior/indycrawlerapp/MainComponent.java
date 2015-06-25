fec28769eb1d8cc973998970e3346b1390173e04
David Britto Jr <brittodavid@yahoo.com>
2015-06-24 23:44:04 -0300

    First sketch of the Event List Screen

--- mobile/app/src/main/java/com/djunior/indycrawlerapp/MainComponent.java
+++ mobile/app/src/main/java/com/djunior/indycrawlerapp/MainComponent.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.HttpHostConnectException;
@@ -21,27 +25,52 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainComponent extends ActionBarActivity {
+
+    SimpleAdapter adapter;
+    ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("IndyCrawlerApp", "OnCreate() begin");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_component);
-        Log.d("IndyCrawlerApp", "OnCreate() calling getEvents");
-        getEvents();
-        Log.d("IndyCrawlerApp", "OnCreate() getEvents called");
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        adapter=new SimpleAdapter(this, list,
+                R.layout.eventlist_element,
+                new String[] { "name","info" },
+                new int[] {R.id.name_entry, R.id.info_entry});
+
+        listView.setAdapter(adapter);
+
+        addEvent("Evento 1", "CCBB - 24/05/2015 a 26/07/2016");
+        addEvent("Evento 2","MAC - 24/05/2015 a 26/07/2016");
+        addEvent("Evento 3","MAR - 24/05/2015 a 26/07/2016");
+        addEvent("Evento 4","MAM - 24/05/2015 a 26/07/2016");
     }
 
+    private void addEvent(String name, String info){
+        HashMap<String,String> event = new HashMap<>();
+        event.put("name",name);
+        event.put("info",info);
+        list.add(event);
+        adapter.notifyDataSetChanged();
+    }
 
     private void getEvents(){
-        Log.d("IndyCrawllerApp","getEvent(), creating NetworkWrapper");
+        Log.d("IndyCrawlerApp","getEvent(), creating NetworkWrapper");
         try {
             Log.d("IndyCrawlerApp","Calling networkwrapper");
             String serverResponse = new NetworkWrapper().execute("http://10.10.1.35:8080/axis2/services/IndyCrawler/getEvents").get();
-            Log.d("IndyCrawllerApp","ServerResponse = " + serverResponse);
+            Log.d("IndyCrawlerApp","ServerResponse = " + serverResponse);
+
+            // TODO - Parse response and build event list
         } catch (Exception e) {
             e.printStackTrace();
         }
